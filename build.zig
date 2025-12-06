eee2b57cacd7054309ca4bcedbae41ebf2659f07
Agustin Catellani <agustin.catellani@gmail.com>
2025-12-06 16:28:08 +0000

    add test step

--- build.zig
+++ build.zig
@@ -6,7 +6,6 @@ pub fn build(b: *std.Build) void {
 
     const day = b.option(u8, "day", "day") orelse 0;
     const day_str = std.fmt.allocPrint(b.allocator, "day{}", .{day}) catch std.process.exit(1);
-
     const challenge_path = std.mem.concat(b.allocator, u8, &[_][]const u8{
         "src/",
         day_str,
@@ -24,6 +23,18 @@ pub fn build(b: *std.Build) void {
     });
 
     b.installArtifact(exe);
+
+    const test_step = b.step("test", "Run unit tests");
+
+    const unit_tests = b.addTest(.{
+        .root_module = b.createModule(.{
+            .root_source_file = b.path(challenge_path),
+            .target = target
+        }),
+    });
+
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    test_step.dependOn(&run_unit_tests.step);
 
     const run_step = b.step("run", "Solve the challenge");
     const run_cmd = b.addRunArtifact(exe);
